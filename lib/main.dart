6a23603bb64f7b297844af4d1e2c5d7ec2fc5e63
Furlan <furlan@pop-os.localdomain>
2020-10-10 12:36:32 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -29,33 +29,133 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
+
+  Widget _buildCard(String header, String content) {
+    return Padding(
+      padding: EdgeInsets.all(2.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.tealAccent,
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    header,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.amberAccent
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Flexible(
+                    child: Text(
+                      content,
+                      overflow: TextOverflow.visible,
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.amberAccent
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+      title: "Some Title",
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Another title"),
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.account_box)),
+                Tab(icon: Icon(Icons.account_circle)),
+                Tab(icon: Icon(Icons.attach_money))
+              ],
             ),
-            Text(
-              '',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
+          body: TabBarView(
+            children: [
+              Center(child: aboutMe()),
+              Center(child: theForm()),
+              Center(child: theProj())
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget aboutMe() {
+    return Center(
+      child: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Some text here",
+                  style: TextStyle(
+                    color: Colors.redAccent,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(5.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.tealAccent,
+                borderRadius: BorderRadius.all(Radius.elliptical(30, 30))
+              ),
+              child: ListView(
+                primary: false,
+                padding: EdgeInsets.all(10.0),
+                children: [
+                  _buildCard("Nome", "Matheus Furlan")
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget theForm() {
+    return null;
+  }
+
+  Widget theProj() {
+    return null;
   }
 }
